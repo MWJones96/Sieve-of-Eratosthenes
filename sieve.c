8a89b5836353dd3e37a542db84bcda9255553df3
Matthew Jones <eighthmatthewjones@gmail.com>
2020-08-25 11:04:39 -0700

    More setup

--- sieve.c
+++ sieve.c
@@ -1,7 +1,11 @@
 #include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+bool get_bit(uint8_t* sieve, int bit);
+void set_bit(uint8_t* sieve, int bit);
 
 int main(int argc, char** argv)
 {
@@ -13,9 +17,27 @@ int main(int argc, char** argv)
 
     uint8_t* sieve = (uint8_t*) malloc(NUM_BYTES * sizeof(uint8_t));
 
-
+    int i;
+    for (i = 0; i < NUM_BITS; i++)
+    {
+        bool b = get_bit(sieve, i);
+    }
 
     free(sieve);
 
     return 0;
 }
+
+bool get_bit(uint8_t* sieve, int bit)
+{
+    int byte = bit / 8;
+    int offset = bit % 8;
+
+    uint8_t sieveByte = *(sieve + byte);
+    printf("Byte Num: %d, value = %d\n", byte, sieveByte);
+
+    return false;
+}
+
+void set_bit(uint8_t* sieve, int bit)
+{}
